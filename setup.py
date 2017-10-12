da5fc2c2493468ce5d8429590951cb8f13e376b7
Michael Fester <michael.fester@gmail.com>
2017-10-12 14:14:24 +0200

    Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snipsskillscore',
-    version='0.1.5.9.2',
+    version='0.1.5.9.3',
     description='The Snips skills core utilities for creating end-to-end assistants',
     author='Michael Fester',
     author_email='michael.fester@gmail.com',
