62e2c3ae814eded8660c5beb306cccd1fcc76026
Michael Fester <michael.fester@gmail.com>
2017-11-08 18:39:47 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snipsmanagercore',
-    version='0.1.5.9.5',
+    version='0.1.5.9.6',
     description='The Snips skills core utilities for creating end-to-end assistants',
     author='Michael Fester',
     author_email='michael.fester@gmail.com',
