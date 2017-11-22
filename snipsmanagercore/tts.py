6a52445224ba95f6e57eaeb450f786b836a8bac7
anthonyray <dantho361@hotmail.com>
2017-11-22 17:00:28 +0100

    working on TTS provider

--- snipsmanagercore/tts.py
+++ snipsmanagercore/tts.py
@@ -15,7 +15,7 @@ from .audio_player import AudioPlayer
 class SnipsTTS:
     """ Snips TTS service. """
 
-    def __init__(self, thread_handler, mqtt_hostname, mqtt_port, mqtt_topic, locale, logger=None):
+    def __init__(self, thread_handler, mqtt_hostname, mqtt_port, locale, session_id, logger=None, dialog_action="endSession"):
         """ Initialise the service.
 
         :param mqtt_hostname: the MQTT broker hostname.
@@ -26,7 +26,7 @@ class SnipsTTS:
         self.logger = logger
         self.locale = locale.split("_")[0]
 
-        self.mqtt_topic = mqtt_topic
+        self.mqtt_topic = "hermes/dialogueManager/{}".format(dialog_action)
         self.mqtt_client = mqtt.Client("SnipsTTS")
         
         thread_handler.run(target=self.start_blocking, args=(mqtt_hostname, mqtt_port, ))
@@ -49,49 +49,10 @@ class SnipsTTS:
 
         if self.logger is not None:
             self.logger.info("Snips TTS: {}".format(sentence))
+
+
         self.mqtt_client.publish(
             self.mqtt_topic,
             payload=json.dumps({'text': sentence}),
             qos=0,
             retain=False)
-
-
-class GTTS:
-    """ Google TTS service. """
-
-    def __init__(self, locale, logger=None):
-        """ Initialise the service.
-
-        :param locale: the language locale, e.g. "fr" or "en_US".
-        """
-        self.logger = logger
-        self.locale = locale.split("_")[0]
-
-    def speak(self, sentence):
-        """ Speak a sentence using Google TTS.
-
-        :param sentence: the sentence to speak.
-        """
-        snips_dir = ".snips"
-        filename = "gtts.mp3"
-        file_path = "{}/{}".format(snips_dir, filename)
-
-        if not os.path.exists(snips_dir):
-            os.makedirs(snips_dir)
-
-        def delete_file():
-            try:
-                os.remove(file_path)
-                if not os.listdir(snips_dir):
-                    try:
-                        os.rmdir(snips_dir)
-                    except OSError:
-                        pass
-            except:
-                pass
-
-        if self.logger is not None:
-            self.logger.info("Google TTS: {}".format(sentence))
-        tts = gTTS(text=sentence, lang=self.locale)
-        tts.save(file_path)
-        AudioPlayer.play_async(file_path, delete_file)
